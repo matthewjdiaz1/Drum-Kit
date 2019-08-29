a38d19bd6f38e3341701af153eeb8b5715314f5c
Matthew-Jacob-Diaz <matthewjdiaz1@gmail.com>
2019-08-29 10:40:56 -0700

    added kick and snare sound file

--- client/components/button.jsx
+++ client/components/button.jsx
@@ -1,73 +1,102 @@
 import React from 'react';
 
+import Tone from 'tone';
+import kickSoundFile from '../../public/assets/sounds/fateKick02.mp3';
+import snareSoundFile from '../../public/assets/sounds/monsterSnareHard1.wav';
+
 class Button extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      classStyle: ""
+      scaleButton: {},
+      kick: null,
+      snare: null,
+      test: null,
     }
     this.handleClick = this.handleClick.bind(this);
     this.handleKeyDown = this.handleKeyDown.bind(this);
   }
 
-  //////////////////////////
-  // TODO - instead of sending thru props, perhaps component mounting would 
-  // listen always and would be able to activate the handle slick with a 
-  // switch statement
-  ///////////////////////////
-
   componentDidMount() {
     document.addEventListener("keydown", this.handleKeyDown, false);
+
+    // let loadKick = new Audio(kickSoundFile);
+    this.setState({ kick: kickSoundFile });
+    this.setState({ snare: snareSoundFile });
+    let testSample = new Tone.Player({
+      "url": this.state.kick,
+      "autostart": true,
+    }).toMaster();
+    this.setState({ test: testSample })
   }
   componentWillUnmount() {
     document.removeEventListener("keydown", this.handleKeyDown, false);
   }
 
-  handleClick() {
-    this.setState({ classStyle: "button-pressed" });
-    setTimeout(() => this.setState({ classStyle: "" }), 70);
+  handleClick(note) {
+    this.setState({ scaleButton: { "transform": "scale(1.1)", "borderColor": "gold" } });
+    setTimeout(() => this.setState({ scaleButton: {} }), 100);
+
+    // var sound = new Audio(require('../../public/assets/sounds/Fate_kick_02.mp3'));
+    // sound.play();
+
+    // var synth = new Tone.Synth().toMaster();
+    // //play a middle 'C' for the duration of an 8th note
+    // synth.triggerAttackRelease(note, '8n')
   }
 
   handleKeyDown(event) {
     switch (event.keyCode) {
       case 65:
         if (this.props.keyPressed === "A") {
-          this.handleClick();
+          this.handleClick("C4");
         }
         break;
       case 83:
         if (this.props.keyPressed === "S") {
-          this.handleClick();
+          this.handleClick("Db4");
+
         }
         break;
       case 68:
+        // kick
         if (this.props.keyPressed === "D") {
-          this.handleClick();
+          this.handleClick("E4");
+          let player = new Tone.Player({
+            "url": this.state.kick,
+            "autostart": true,
+          }).toMaster();
         }
         break;
       case 70:
         if (this.props.keyPressed === "F") {
-          this.handleClick();
+          this.handleClick("F4");
+          console.log('test');
+          this.state.test.start();
         }
         break;
       case 71:
         if (this.props.keyPressed === "G") {
-          this.handleClick();
+          this.handleClick("G4");
         }
         break;
       case 72:
         if (this.props.keyPressed === "H") {
-          this.handleClick();
+          this.handleClick("Ab4");
         }
         break;
       case 74:
         if (this.props.keyPressed === "J") {
-          this.handleClick();
+          this.handleClick("B4");
+          let player = new Tone.Player({
+            "url": this.state.snare,
+            "autostart": true,
+          }).toMaster();
         }
         break;
       case 75:
         if (this.props.keyPressed === "K") {
-          this.handleClick();
+          this.handleClick("C5");
         }
         break;
       case 76:
@@ -83,11 +112,11 @@ class Button extends React.Component {
   render() {
     return (
       <div onKeyDown={() => this.handleKeyDown(event)}>
-        <button className={this.state.classStyle} onClick={() => this.handleClick()}>
+        <button style={this.state.scaleButton} onClick={() => this.handleClick()}>
           <div style={{ "fontWeight": "bold", "fontSize": "20px" }}>{this.props.keyPressed}</div>
           <div style={{ "color": "gold", "textShadow": "-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black" }}>{this.props.sound}</div>
         </button>
-      </div>
+      </div >
     )
   }
 }
--- server/index.js
+++ server/index.js
@@ -4,5 +4,9 @@ const port = 3000;
 
 // app.get('/', (req, res) => res.send('from express server'));
 app.use(express.static('public'));
+// app.get('/public/assets/sounds/Fate_kick_02.mp3', (req, res) => {
+//   res.send('test');
+//   console.log('test');
+// });
 
 app.listen(port, () => console.log(`App listening on port ${port}.`));
